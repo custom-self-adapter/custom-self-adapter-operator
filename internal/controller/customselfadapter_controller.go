21067c74bcf5661e1333519f7a7bda63e5fc687f
Humberto Fraga <humbertofraga@gmail.com>
2025-07-05 07:58:17 -0300

    Adds RBAC markers for the controller

--- internal/controller/customselfadapter_controller.go
+++ internal/controller/customselfadapter_controller.go
@@ -100,14 +100,14 @@ type CustomSelfAdapterReconciler struct {
 // +kubebuilder:rbac:groups=custom-self-adapter.net,resources=customselfadapters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=custom-self-adapter.net,resources=customselfadapters/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=custom-self-adapter.net,resources=customselfadapters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=pods,verbs=*
+// +kubebuilder:rbac:groups="",resources=replicationcontrollers;replicationcontrollers/scale,verbs=*
+// +kubebuilder:rbac:groups=apps,resources=replicasets;replicasets/scale,verbs=*
+// +kubebuilder:rbac:groups=apps,resources=statefulsets;statefulsets/scale,verbs=*
+// +kubebuilder:rbac:groups=apps,resources=deployments;deployments/scale,verbs=*
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the CustomSelfAdapter object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *CustomSelfAdapterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
